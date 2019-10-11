78cb262054a5f57241eec86e4e201c20b2ba788f
th92rodr <45070690+th92rodr@users.noreply.github.com>
2019-10-11 12:10:17 -0300

    add the routes to the express app instance

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
+
+app.use('/', routes);
 
 const PORT = 5000;
 const HOST = '0.0.0.0';
 
 const server = app.listen(PORT, HOST, () => {
-    console.log(`Server is running on port ${server.address().port}`);
+  console.log(`Server is running on port ${server.address().port}`);
 });
