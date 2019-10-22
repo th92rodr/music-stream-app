307c98813e4ee9bb84818624e6584f7b43c85564
th92rodr <45070690+th92rodr@users.noreply.github.com>
2019-10-21 23:08:15 -0300

    add associate method in music model

--- models/Music.js
+++ models/Music.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = require('../database');
+const db = require('../database/database');
 
 const Music = db.define(
   'music',
@@ -19,17 +19,30 @@ const Music = db.define(
     }
   },
   {
+    // explicitly tell Sequelize that this model is linked
+    // to a table named 'musics' instead of having Sequelize
+    // automatically determine table names, which can be error prone
     tableName: 'musics',
     underscored: true,
 
     classMethods: {
       associate: function(models) {
         Music.belongsTo(models.Album, {
-          foreignKey: {
-            allowNull: false
-          },
+          foreignKey: 'album_id',
+          as: 'album',
           onDelete: 'CASCADE'
         });
+      }
+    },
+
+    instanceMethods: {
+      standardRes: () => {
+        return {
+          id: this.id,
+          title: this.title,
+          duration: this.duration,
+          file: this.file
+        };
       }
     }
   }
