815f90eb42b9eb02dc28ba80f7eec394f9638cbd
th92rodr <45070690+th92rodr@users.noreply.github.com>
2019-10-29 22:42:03 -0300

    Add artist show method

--- controllers/artist.js
+++ controllers/artist.js
@@ -9,5 +9,16 @@ module.exports = {
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
-  }
+  },
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      const artist = await Artist.findByPk(id);
+
+      return res.status(200).json({ artist });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  },
 };
