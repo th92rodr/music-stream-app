50d4d12edfbc9862a10469ba9d14bb0c78584476
th92rodr <45070690+th92rodr@users.noreply.github.com>
2019-11-16 14:14:02 -0300

    add index view method to music controller

--- src/controllers/music_controller.js
+++ src/controllers/music_controller.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const { Album, Music } = require('../models/index');
 
@@ -76,12 +77,15 @@ module.exports = {
   },
 
   async stream(req, res) {
-    const { id } = req.params;
+    //const { id } = req.params;
 
-    const music = await Music.findByPk(id);
+    //const music = await Music.findByPk(id);
 
-    const stat = await getStat(music.file);
+    //const stat = await getStat(music.file);
     //console.log(stat);
+
+    const filePath = path.resolve(__dirname, '..', '..', 'The_Price_of_a_Mile.mp3');
+    const stat = await getStat(filePath);
 
     // informações sobre o tipo do conteúdo e o tamanho do arquivo
     res.writeHead(200, {
@@ -89,7 +93,8 @@ module.exports = {
       'Content-Length': stat.size
     });
 
-    const stream = fs.createReadStream(music.file);
+    //const stream = fs.createReadStream(music.file);
+    const stream = fs.createReadStream(filePath);
 
     // faz streaming do audio
     stream.pipe(res);
@@ -105,5 +110,64 @@ module.exports = {
         res.end();
     });
     */
-  }
+  },
+
+
+  async index_view(req, res) {
+    try {
+      /*
+      const { album_id } = req.params;
+
+      const album = await Album.findByPk(album_id, {
+        include: { association: 'tracks' }
+      });
+      */
+
+      const album = {
+        id: 4,
+        name: 'Brave New World',
+        year: '2000',
+        cover: '',
+        tracks: [
+          {
+            id: 1,
+            title: 'The Wicker Man',
+            duration: '4:35',
+            number: 1
+          },
+          {
+            id: 2,
+            title: 'Ghost of the Navigator',
+            duration: '6:50',
+            number: 2
+          },
+          {
+            id: 3,
+            title: 'Brave New World',
+            duration: '6:!9',
+            number: 3
+          },
+          {
+            id: 4,
+            title: 'Blood Brothers',
+            duration: '7:12',
+            number: 4
+          },
+          {
+            id: 5,
+            title: 'Dream Of Mirrors',
+            duration: '9:20',
+            number: 5
+          },
+        ]
+      };
+
+      return res.status(200).render('music', {
+        album
+      });
+    } catch (error) {
+      return res.status(500).render('404');
+    }
+  },
+
 };
