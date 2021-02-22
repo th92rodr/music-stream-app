3aac1766b88af3588053de736226be3492474847
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-02-22 11:12:40 -0300

    add delete method in users controller

--- api/src/channels/rest/express/controllers/UsersController.ts
+++ api/src/channels/rest/express/controllers/UsersController.ts
@@ -3,16 +3,19 @@ import { Request, Response } from 'express';
 import CreateUserService from '../../../../services/CreateUserService';
 import GetUserService from '../../../../services/GetUserService';
 import UpdateUserService from '../../../../services/UpdateUserService';
+import DeleteUserService from '../../../../services/DeleteUserService';
 
 export default class UsersController {
   private createUserService: CreateUserService;
   private getUserService: GetUserService;
   private updateUserService: UpdateUserService;
+  private deleteUserService: DeleteUserService;
 
   constructor() {
     this.createUserService = new CreateUserService();
     this.getUserService = new GetUserService();
     this.updateUserService = new UpdateUserService();
+    this.deleteUserService = new DeleteUserService();
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
@@ -47,5 +50,13 @@ export default class UsersController {
     });
 
     return response.json(user);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const id = request.user.id;
+
+    await this.deleteUserService.execute({ id });
+
+    return response.send();
   }
 }
