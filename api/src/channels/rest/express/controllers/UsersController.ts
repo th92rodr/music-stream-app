80bed9688f94a93f0b56790eafe3ae8c545e7b53
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-02-22 11:10:19 -0300

    add update method in users controller

--- api/src/channels/rest/express/controllers/UsersController.ts
+++ api/src/channels/rest/express/controllers/UsersController.ts
@@ -2,14 +2,17 @@ import { Request, Response } from 'express';
 
 import CreateUserService from '../../../../services/CreateUserService';
 import GetUserService from '../../../../services/GetUserService';
+import UpdateUserService from '../../../../services/UpdateUserService';
 
 export default class UsersController {
   private createUserService: CreateUserService;
   private getUserService: GetUserService;
+  private updateUserService: UpdateUserService;
 
   constructor() {
     this.createUserService = new CreateUserService();
     this.getUserService = new GetUserService();
+    this.updateUserService = new UpdateUserService();
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
@@ -28,6 +31,20 @@ export default class UsersController {
     const id = request.user.id;
 
     const user = await this.getUserService.execute({ id });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const id = request.user.id;
+    const { username, email, password } = request.body;
+
+    const user = await this.updateUserService.execute({
+      id,
+      username,
+      email,
+      password,
+    });
 
     return response.json(user);
   }
