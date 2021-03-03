f27fb055e47f6533bfe377d8c692ea43249dbc05
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-03 20:11:16 -0300

    musics-service: update musics repository

--- apis/music-service/src/repositories/MusicsRepository/interface.ts
+++ apis/music-service/src/repositories/MusicsRepository/interface.ts
@@ -1,8 +1,8 @@
 import Music from '@entities/Music';
 
 export default interface MusicsRepository {
-  find(id: string): Promise<Music>;
+  find(id: string): Promise<Music | undefined>;
   store(music: Music): Promise<void>;
-  update(music: Music): Promise<Music>;
+  update(music: Music): Promise<void>;
   delete(id: string): Promise<void>;
 }
