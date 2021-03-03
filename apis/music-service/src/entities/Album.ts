472f4a4fdf9689e5673babe0a352ccd31b030781
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-03 20:10:41 -0300

    musics-service: update entities

--- apis/music-service/src/entities/Album.ts
+++ apis/music-service/src/entities/Album.ts
@@ -7,16 +7,23 @@ export default class Album {
   public readonly cover: string;
   public readonly studio: string;
   public readonly producers: Array<string>;
-  public readonly tracks: Array<Music>;
-  public readonly tracksIds: Array<string>;
+  public tracks: Array<Music>;
+  public tracksIds: Array<string>;
+  public readonly artistId: string;
 
-  constructor({ id, name, year, cover, studio, producers, tracks }: Omit<Album, 'tracksIds'>) {
+  constructor({ id, name, year, cover, studio, producers, artistId }: Omit<Album, 'tracks' | 'tracksIds' | 'setTracks'>) {
     this.id = id;
     this.name = name;
     this.year = year;
     this.cover = cover;
     this.studio = studio;
     this.producers = producers;
+    this.artistId = artistId;
+    this.tracks = [];
+    this.tracksIds = [];
+  }
+
+  public setTracks(tracks: Array<Music>) {
     this.tracks = tracks;
     this.tracksIds = tracks.map(track => track.id);
   }
--- apis/music-service/src/entities/Artist.ts
+++ apis/music-service/src/entities/Artist.ts
@@ -7,15 +7,20 @@ export default class Artist {
   public readonly description: string;
   public readonly genre: Genre;
   public readonly photos: Array<string>;
-  public readonly albums: Array<Album>;
-  public readonly albumsIds: Array<string>;
+  public albums: Array<Album>;
+  public albumsIds: Array<string>;
 
-  constructor({ id, name, description, genre, photos, albums }: Omit<Artist, 'albumsIds'>) {
+  constructor({ id, name, description, genre, photos }: Omit<Artist, 'albums' | 'albumsIds' | 'setAlbums'>) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.genre = genre;
     this.photos = photos;
+    this.albums = [];
+    this.albumsIds = [];
+  }
+
+  public setAlbums(albums: Array<Album>) {
     this.albums = albums;
     this.albumsIds = albums.map(album => album.id);
   }
