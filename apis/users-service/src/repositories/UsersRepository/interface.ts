8a5018b9a687b511c7cdb32320ae4c6fcb5020c1
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-04 18:32:33 -0300

    update users repository

--- apis/users-service/src/repositories/UsersRepository/interface.ts
+++ apis/users-service/src/repositories/UsersRepository/interface.ts
@@ -1,9 +1,9 @@
 import User from '@entities/User';
 
 export default interface UsersRepository {
-  find(id: string): Promise<User>;
-  findByEmail(email: string): Promise<User>;
+  find(id: string): Promise<User | undefined>;
+  findByEmail(email: string): Promise<User | undefined>;
   store(user: User): Promise<void>;
-  update(user: User): Promise<User>;
+  update(user: User): Promise<void>;
   delete(id: string): Promise<void>;
 }
