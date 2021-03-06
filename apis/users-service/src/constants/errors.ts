75b94746c6c5bf0b16717ab5ad55deee3a485959
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-06 18:47:40 -0300

    users-service: add some custom errors in users-service

---
+++ apis/users-service/src/constants/errors.ts
@@ -0,0 +1,46 @@
+import { StatusCode } from './index';
+import BaseError from '@errors/BaseError';
+
+export class ErrorUserNotFound extends BaseError {
+  constructor(id: string | null, email: string | null) {
+    super({
+      name: 'NOT FOUND',
+      statusCode: StatusCode.NOT_FOUND,
+      isOperational: true,
+      message: id != null ? `An user entity with the id ${id} was not found.` : `An user entity with the email ${email} was not found.`,
+    });
+  }
+}
+
+export class ErrorInvalidCredentials extends BaseError {
+  constructor() {
+    super({
+      name: 'INVALID CREDENTIALS',
+      statusCode: StatusCode.BAD_REQUEST,
+      isOperational: true,
+      message: 'Invalid credentials.',
+    });
+  }
+}
+
+export class ErrorEmailInUse extends BaseError {
+  constructor(email: string) {
+    super({
+      name: 'INVALID CREDENTIALS',
+      statusCode: StatusCode.BAD_REQUEST,
+      isOperational: true,
+      message: `The email ${email} is already in use.`,
+    });
+  }
+}
+
+export class InternalError extends BaseError {
+  constructor() {
+    super({
+      name: 'INTERNAL ERROR',
+      statusCode: StatusCode.INTERNAL_SERVER,
+      isOperational: false,
+      message: `An unexpected error occur.`,
+    });
+  }
+}
