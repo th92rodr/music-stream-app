b281a58c8197b13605b17a0ac3c053cfa03fd671
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-08 21:19:22 -0300

    refactor users-service

---
+++ apis/users-service/src/services/dtos.ts
@@ -0,0 +1,32 @@
+import User from '@entities/User';
+
+export interface GetUserRequest {
+  id: string;
+}
+
+export interface CreateUserRequest {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface UpdateUserRequest {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface DeleteUserRequest {
+  id: string;
+}
+
+export interface AuthenticateUserRequest {
+  email: string;
+  password: string;
+}
+
+export interface AuthenticateUserResponse {
+  user: User;
+  token: string;
+}
