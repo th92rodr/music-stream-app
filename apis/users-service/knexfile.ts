b8e9ff94f8e397f4121e950284126f598e03f3af
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-12 15:59:27 -0300

    update knexfile

--- apis/users-service/knexfile.ts
+++ apis/users-service/knexfile.ts
@@ -1,10 +1,11 @@
+import 'dotenv/config';
 import path from 'path';
 
 module.exports = {
   client: 'pg',
   connection: process.env.POSTGRES_ADDRESS,
   migrations: {
-    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    directory: path.resolve(__dirname, 'src', 'database', 'SQL', 'migrations'),
   },
   useNullAsDefault: true,
 };
