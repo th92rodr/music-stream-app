acf8231e54d16cd18385421c9447bd89696d32d7
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-12 16:45:41 -0300

    Start grpc channel

--- apis/users-service/src/main.ts
+++ apis/users-service/src/main.ts
@@ -1,1 +1,15 @@
 import 'dotenv/config';
+
+import { grpcChannel } from '@channels/index';
+import { database } from '@database/index';
+import { errorHandler } from '@handlers/index';
+import { loggerProvider } from '@providers/index';
+
+grpcChannel.start();
+
+async function shutdown(exitCode = 0): Promise<void> {
+  await grpcChannel.stop();
+  await database.close();
+
+  process.exit(exitCode);
+}
