1c12f37360bbe2b8779e6bde4bbe6b1c8aa7332d
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-12 16:47:06 -0300

    Catch unhandled exceptions and rejections

--- apis/musics-service/src/main.ts
+++ apis/musics-service/src/main.ts
@@ -7,6 +7,20 @@ import { loggerProvider } from '@providers/index';
 
 grpcChannel.start();
 
+process.on('unhandledRejection', (error: Error, promise: Promise<any>) => {
+  loggerProvider.info('Unhandled rejection caught.');
+  throw error;
+});
+
+process.on('uncaughtException', (error: Error) => {
+  loggerProvider.info('Unhandled exception caught.');
+
+  errorHandler.handleError(error);
+  if (!errorHandler.isTrustedError(error)) {
+    shutdown(1);
+  }
+});
+
 async function shutdown(exitCode = 0): Promise<void> {
   await grpcChannel.stop();
   await database.close();
