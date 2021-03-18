f891dc7dbfd409d6c01785d9084b7a33963dcfdf
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-18 18:48:47 -0300

    create logger-provider

---
+++ apis/backend-for-frontend/src/providers/LoggerProvider/interface.ts
@@ -0,0 +1,7 @@
+export default interface ILoggerProvider {
+  info(message: string, meta?: any): void;
+  error(message: string, error: Error): void;
+  warn(message: string, meta: Error): void;
+  fatal(message: string, meta: Error): void;
+  debug(message: string, meta?: any): void;
+}
