493f0c1fcc0f3449cec20884570c8ad9629596c3
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-18 20:21:43 -0300

    bff: add shutdown function

--- apis/backend-for-frontend/src/main.ts
+++ apis/backend-for-frontend/src/main.ts
@@ -3,3 +3,9 @@ import 'dotenv/config';
 import { restChannel } from '@channels/index';
 
 restChannel.start();
+
+async function shutdown(exitCode = 0): Promise<void> {
+  await restChannel.stop();
+
+  process.exit(exitCode);
+}
