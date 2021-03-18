6136a1c18c2a42595536de0bfc254a9bff3da161
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-18 20:23:01 -0300

    bff: properly handle SIGINT and SIGTERM

--- apis/backend-for-frontend/src/main.ts
+++ apis/backend-for-frontend/src/main.ts
@@ -1,8 +1,21 @@
 import 'dotenv/config';
 
 import { restChannel } from '@channels/index';
+import { loggerProvider } from '@providers/index';
 
 restChannel.start();
+
+// Properly handle SIGINT and SIGTERM
+
+process.on('SIGINT', function onSigint() {
+  loggerProvider.info('Got SIGINT. Graceful shutdown.');
+  shutdown();
+});
+
+process.on('SIGTERM', function onSigterm() {
+  loggerProvider.info('Got SIGTERM. Graceful shutdown.');
+  shutdown();
+});
 
 async function shutdown(exitCode = 0): Promise<void> {
   await restChannel.stop();
