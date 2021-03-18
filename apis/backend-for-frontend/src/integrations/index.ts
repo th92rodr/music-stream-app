7b5a4b3e990450e2dde2c294c2a19b9bd60c5e5c
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-18 18:54:16 -0300

    bff: add integrations index.ts

---
+++ apis/backend-for-frontend/src/integrations/index.ts
@@ -0,0 +1,6 @@
+import MusicsIntegration from './MusicsIntegration/MusicsIntegration';
+import UsersIntegration from './UsersIntegration/UsersIntegration';
+
+export const musicsIntegration = new MusicsIntegration();
+
+export const usersIntegration = new UsersIntegration();
