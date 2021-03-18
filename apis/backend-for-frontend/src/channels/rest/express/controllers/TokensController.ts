6755796f6ba47b6f5dcc80ea2fa4147e718e8a06
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-18 19:06:45 -0300

    create tokens-controller class

---
+++ apis/backend-for-frontend/src/channels/rest/express/controllers/TokensController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+
+import { HttpStatusCode } from '@constants/index';
+import BaseError from '@constants/BaseError';
+import { InternalError } from '@constants/errors';
+import IUsersIntegration from '@integrations/UsersIntegration/interface';
+
+export default class TokensController {
+  private usersIntegration: IUsersIntegration;
+
+  constructor(usersIntegration: IUsersIntegration) {
+    this.usersIntegration = usersIntegration;
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    try {
+      const { token } = await this.usersIntegration.authenticateUser({ email, password });
+      return response.status(HttpStatusCode.OK).json(token);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+}
