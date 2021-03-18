820abab5cef89e5fce92a172061f41a5c40fd340
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-18 19:05:03 -0300

    create users-controller class

---
+++ apis/backend-for-frontend/src/channels/rest/express/controllers/UsersController.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from 'express';
+
+import { HttpStatusCode } from '@constants/index';
+import BaseError from '@constants/BaseError';
+import IUsersIntegration from '@integrations/UsersIntegration/interface';
+import { InternalError } from '@constants/errors';
+
+export default class UsersController {
+  private usersIntegration: IUsersIntegration;
+
+  constructor(usersIntegration: IUsersIntegration) {
+    this.usersIntegration = usersIntegration;
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const id = request.userId;
+
+    try {
+      const user = await this.usersIntegration.getUser(id);
+      return response.status(HttpStatusCode.OK).json(user);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { username, email, password } = request.body;
+
+    try {
+      const user = await this.usersIntegration.createUser({ username, email, password });
+      return response.status(HttpStatusCode.CREATED).json(user);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const id = request.userId;
+    const { username, email, password } = request.body;
+
+    try {
+      const user = await this.usersIntegration.updateUser({ id, username, email, password });
+      return response.status(HttpStatusCode.OK).json(user);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const id = request.userId;
+
+    try {
+      await this.usersIntegration.deleteUser(id);
+      return response.status(HttpStatusCode.OK).send();
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+}
