0125b98f7e66151ee9c4b8555352d308d73a2433
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-20 20:22:07 -0300

    implement crud in artists-service

--- apis/musics-service/src/services/ArtistsService/ArtistsService.ts
+++ apis/musics-service/src/services/ArtistsService/ArtistsService.ts
@@ -1,14 +1,29 @@
-import { GetArtistRequest } from './dtos';
+// prettier-ignore
+import {
+  GetArtistRequest,
+  GetArtistByGenreRequest,
+  CreateArtistRequest,
+  UpdateArtistRequest,
+  DeleteArtistRequest
+} from './dtos';
 import IArtistsService from './interface';
 import { ErrorArtistNotFound } from '@constants/errors';
 import Artist from '@entities/Artist';
+import IIdProvider from '@providers/IdProvider/interface';
 import IArtistsRepository from '@repositories/ArtistsRepository/interface';
+import { arrayIntersection } from '@utils/index';
 
 export default class ArtistsService implements IArtistsService {
   private artistsRepository: IArtistsRepository;
+  private idProvider: IIdProvider;
 
-  constructor(artistsRepository: IArtistsRepository) {
+  // prettier-ignore
+  constructor(
+    artistsRepository: IArtistsRepository,
+    idProvider: IIdProvider,
+  ) {
     this.artistsRepository = artistsRepository;
+    this.idProvider = idProvider;
   }
 
   public async get({ id }: GetArtistRequest): Promise<Artist> {
@@ -21,9 +36,53 @@ export default class ArtistsService implements IArtistsService {
     return artist;
   }
 
-  public async create(request: any): Promise<void> {}
+  public async getAll(): Promise<Array<Artist>> {
+    const artists = await this.artistsRepository.findAll();
 
-  public async update(request: any): Promise<void> {}
+    return artists;
+  }
 
-  public async delete(request: any): Promise<void> {}
+  public async getByGenre({ genre }: GetArtistByGenreRequest): Promise<Array<Artist>> {
+    const artists = await this.artistsRepository.findByGenre(genre);
+
+    return artists;
+  }
+
+  public async create({ name, description, genre, photos }: CreateArtistRequest): Promise<Artist> {
+    const artist = new Artist({
+      id: this.idProvider.generate(),
+      name,
+      description,
+      genre,
+      photos,
+    });
+
+    await this.artistsRepository.store(artist);
+
+    return artist;
+  }
+
+  public async update({ id, name, description, genre, photos }: UpdateArtistRequest): Promise<Artist> {
+    const artist = await this.artistsRepository.find(id);
+
+    if (!artist) {
+      throw new ErrorArtistNotFound(id);
+    }
+
+    const newArtist = new Artist({
+      id,
+      name: name != '' ? name : artist.name,
+      description: description != '' ? description : artist.description,
+      genre: genre != null ? genre : artist.genre,
+      photos: arrayIntersection(photos, artist.photos),
+    });
+
+    await this.artistsRepository.update(newArtist);
+
+    return newArtist;
+  }
+
+  public async delete({ id }: DeleteArtistRequest): Promise<void> {
+    await this.artistsRepository.delete(id);
+  }
 }
