8c2a25a3ab3bc6c93abb33bd66b5617af380bd09
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-20 20:13:45 -0300

    add array-intersection util function

--- apis/musics-service/src/utils/index.ts
+++ apis/musics-service/src/utils/index.ts
@@ -1,1 +1,5 @@
-export const removeUndefineds = <S>(value: S | undefined): value is S => value != undefined;
+export const removeUndefineds = <T>(value: T | undefined): value is T => value != undefined;
+
+export const arrayIntersection = <T>(array1: Array<T>, array2: Array<T>): Array<T> => {
+  return array1.filter(element => array2.includes(element));
+};
