108ec7326894d6e0ff20ae55294e35bb1603cafc
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-21 14:40:17 -0300

    bff: add musics-integration interface and dtos

--- apis/backend-for-frontend/src/integrations/MusicsIntegration/interface.ts
+++ apis/backend-for-frontend/src/integrations/MusicsIntegration/interface.ts
@@ -1,9 +1,38 @@
+// prettier-ignore
+import {
+  GetMusic,
+  CreateMusic,
+  UpdateMusic,
+  DeleteMusic,
+  GetAlbum,
+  CreateAlbum,
+  UpdateAlbum,
+  DeleteAlbum,
+  GetArtist,
+  GetArtistByGenre,
+  CreateArtist,
+  UpdateArtist,
+  DeleteArtist,
+} from './dtos';
 import Album from '@entities/Album';
 import Artist from '@entities/Artist';
 import Music from '@entities/Music';
 
 export default interface IMusicsIntegration {
-  getMusic(id: string): Promise<Music>;
-  getAlbum(id: string): Promise<Album>;
-  getArtist(id: string): Promise<Artist>;
+  getMusic(request: GetMusic): Promise<Music>;
+  createMusic(request: CreateMusic): Promise<Music>;
+  updateMusic(request: UpdateMusic): Promise<Music>;
+  deleteMusic(request: DeleteMusic): Promise<void>;
+
+  getAlbum(request: GetAlbum): Promise<Album>;
+  createAlbum(request: CreateAlbum): Promise<Album>;
+  updateAlbum(request: UpdateAlbum): Promise<Album>;
+  deleteAlbum(request: DeleteAlbum): Promise<void>;
+
+  getArtist(request: GetArtist): Promise<Artist>;
+  getAllArtists(): Promise<Array<Artist>>;
+  getArtistsByGenre(request: GetArtistByGenre): Promise<Array<Artist>>;
+  createArtist(request: CreateArtist): Promise<Artist>;
+  updateArtist(request: UpdateArtist): Promise<Artist>;
+  deleteArtist(request: DeleteArtist): Promise<void>;
 }
