b25931135de5d5c0d18be19d89c82831860b17d2
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-23 19:09:40 -0300

    bff: implement users-integration proto methods

--- apis/backend-for-frontend/src/integrations/UsersIntegration/UsersIntegration.ts
+++ apis/backend-for-frontend/src/integrations/UsersIntegration/UsersIntegration.ts
@@ -1,22 +1,28 @@
 import * as grpc from 'grpc';
 
 import { UsersClient } from './proto/users_service_grpc_pb';
+// prettier-ignore
 import {
+  User,
+  Id,
+  Empty,
+  CreateUserRequest,
+  UpdateUserRequest,
   AuthenticateUserRequest,
   AuthenticateUserResponse,
-  CreateUserRequest,
-  CreateUserResponse,
-  DeleteUserRequest,
-  DeleteUserResponse,
-  GetUserRequest,
-  GetUserResponse,
-  UpdateUserRequest,
-  UpdateUserResponse,
 } from './proto/users_service_pb';
-import { AuthenticateRequest, AuthenticateResponse, CreateRequest } from './dtos';
+// prettier-ignore
+import {
+  GetUser,
+  CreateUser,
+  UpdateUser,
+  DeleteUser,
+  AuthenticateUser,
+  AuthenticateResponse,
+} from './dtos';
 import IUsersIntegration from './interface';
 import Config from '@config/index';
-import User from '@entities/User';
+import UserEntity from '@entities/User';
 
 export default class UsersIntegration implements IUsersIntegration {
   private client: UsersClient;
@@ -27,60 +33,60 @@ export default class UsersIntegration implements IUsersIntegration {
     this.client = new UsersClient(ADDRESS, grpc.credentials.createInsecure());
   }
 
-  public getUser = async (id: string): Promise<User> => {
+  public getUser = async ({ id }: GetUser): Promise<UserEntity> => {
     return new Promise((resolve, reject) => {
-      const getUserRequest = new GetUserRequest();
-      getUserRequest.setId(id);
+      const userId = new Id();
+      userId.setId(id);
 
-      this.client.get(getUserRequest, (error: Error | null, response: GetUserResponse) => {
+      this.client.get(userId, (error: Error | null, user: User) => {
         if (error != null) reject(error);
-        else resolve(this.translateUserEntity(response));
+        else resolve(this.translateUserEntity(user));
       });
     });
   };
 
-  public createUser = async ({ username, email, password }: CreateRequest): Promise<User> => {
+  public createUser = async ({ username, email, password }: CreateUser): Promise<UserEntity> => {
     return new Promise((resolve, reject) => {
       const createUserRequest = new CreateUserRequest();
       createUserRequest.setUsername(username);
       createUserRequest.setEmail(email);
       createUserRequest.setPassword(password);
 
-      this.client.create(createUserRequest, (error: Error | null, response: CreateUserResponse) => {
+      this.client.create(createUserRequest, (error: Error | null, user: User) => {
         if (error != null) reject(error);
-        else resolve(this.translateUserEntity(response));
+        else resolve(this.translateUserEntity(user));
       });
     });
   };
 
-  public updateUser = async ({ id, username, email, password }: User): Promise<User> => {
+  public updateUser = async ({ id, username, email, password }: UpdateUser): Promise<UserEntity> => {
     return new Promise((resolve, reject) => {
       const updateUserRequest = new UpdateUserRequest();
       updateUserRequest.setId(id);
-      updateUserRequest.setUsername(username);
-      updateUserRequest.setEmail(email);
-      updateUserRequest.setPassword(password);
+      updateUserRequest.setUsername(username ? username : '');
+      updateUserRequest.setEmail(email ? email : '');
+      updateUserRequest.setPassword(password ? password : '');
 
-      this.client.update(updateUserRequest, (error: Error | null, response: UpdateUserResponse) => {
+      this.client.update(updateUserRequest, (error: Error | null, user: User) => {
         if (error != null) reject(error);
-        else resolve(this.translateUserEntity(response));
+        else resolve(this.translateUserEntity(user));
       });
     });
   };
 
-  public deleteUser = async (id: string): Promise<void> => {
+  public deleteUser = async ({ id }: DeleteUser): Promise<void> => {
     return new Promise((resolve, reject) => {
-      const deleteUserRequest = new DeleteUserRequest();
-      deleteUserRequest.setId(id);
+      const userId = new Id();
+      userId.setId(id);
 
-      this.client.delete(deleteUserRequest, (error: Error | null, response: DeleteUserResponse) => {
+      this.client.delete(userId, (error: Error | null) => {
         if (error != null) reject(error);
         else resolve();
       });
     });
   };
 
-  public authenticateUser = async ({ email, password }: AuthenticateRequest): Promise<AuthenticateResponse> => {
+  public authenticateUser = async ({ email, password }: AuthenticateUser): Promise<AuthenticateResponse> => {
     return new Promise((resolve, reject) => {
       const authenticateUserRequest = new AuthenticateUserRequest();
       authenticateUserRequest.setEmail(email);
@@ -88,13 +94,19 @@ export default class UsersIntegration implements IUsersIntegration {
 
       this.client.authenticate(authenticateUserRequest, (error: Error | null, response: AuthenticateUserResponse) => {
         if (error != null) reject(error);
-        else resolve({ token: response.getToken() });
+        else {
+          const user = response.getUser();
+          resolve({
+            token: response.getToken(),
+            user: user ? this.translateUserEntity(user) : undefined,
+          });
+        }
       });
     });
   };
 
-  private translateUserEntity(user: GetUserResponse | CreateUserResponse | UpdateUserResponse): User {
-    return new User({
+  private translateUserEntity(user: User): UserEntity {
+    return new UserEntity({
       id: user.getId(),
       username: user.getUsername(),
       email: user.getEmail(),
