4bd520882933c477490fa4d5902e71b67aac725a
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-23 19:16:52 -0300

    create albums rest controller

---
+++ apis/backend-for-frontend/src/channels/rest/express/controllers/AlbumsController.ts
@@ -0,0 +1,91 @@
+import { Request, Response } from 'express';
+
+import { HttpStatusCode } from '@constants/index';
+import BaseError from '@constants/BaseError';
+import { InternalError } from '@constants/errors';
+import IMusicsIntegration from '@integrations/MusicsIntegration/interface';
+
+export default class AlbumsController {
+  private musicsIntegration: IMusicsIntegration;
+
+  constructor(musicsIntegration: IMusicsIntegration) {
+    this.musicsIntegration = musicsIntegration;
+  }
+
+  public async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    try {
+      const album = await this.musicsIntegration.getAlbum({ id });
+
+      return response.status(HttpStatusCode.OK).json(album);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async create(request: Request, response: Response) {
+    const { name, year, cover, studio, producers, artistId } = request.body;
+
+    try {
+      const album = await this.musicsIntegration.createAlbum({ name, year, cover, studio, producers, artistId });
+
+      return response.status(HttpStatusCode.CREATED).json(album);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { name, year, cover, studio, producers, artistId } = request.body;
+
+    try {
+      const album = await this.musicsIntegration.updateAlbum({ id, name, year, cover, studio, producers, artistId });
+
+      return response.status(HttpStatusCode.OK).json(album);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async delete(request: Request, response: Response) {
+    const { id } = request.params;
+
+    try {
+      await this.musicsIntegration.deleteAlbum({ id });
+
+      return response.status(HttpStatusCode.OK).send();
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+}
