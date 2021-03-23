a68cfd6abe7e40307b90c654e46c04e786141fba
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-23 19:16:18 -0300

    add crud methods in musics rest controller

--- apis/backend-for-frontend/src/channels/rest/express/controllers/MusicsController.ts
+++ apis/backend-for-frontend/src/channels/rest/express/controllers/MusicsController.ts
@@ -20,7 +20,7 @@ export default class MusicsController {
     const { id } = request.params;
 
     try {
-      const music = await this.musicsIntegration.getMusic(id);
+      const music = await this.musicsIntegration.getMusic({ id });
 
       const storagePath = Config.storage.path;
       const musicFile = path.resolve(storagePath, music.file);
@@ -34,6 +34,83 @@ export default class MusicsController {
 
       const stream = fs.createReadStream(musicFile);
       stream.pipe(response);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    try {
+      const music = await this.musicsIntegration.getMusic({ id });
+
+      return response.status(HttpStatusCode.OK).json(music);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async create(request: Request, response: Response) {
+    const { title, durationInSeconds, file, composers, lyrics, albumId } = request.body;
+
+    try {
+      const music = await this.musicsIntegration.createMusic({ title, durationInSeconds, file, composers, lyrics, albumId });
+
+      return response.status(HttpStatusCode.CREATED).json(music);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const { title, durationInSeconds, file, composers, lyrics, albumId } = request.body;
+
+    try {
+      const music = await this.musicsIntegration.updateMusic({ id, title, durationInSeconds, file, composers, lyrics, albumId });
+
+      return response.status(HttpStatusCode.OK).json(music);
+
+      //
+    } catch (error) {
+      if (error instanceof BaseError) {
+        return response.status(error.statusCode).json({ error: error.message });
+      }
+
+      const internalError = new InternalError();
+      return response.status(internalError.statusCode).json({ error: internalError.message });
+    }
+  }
+
+  public async delete(request: Request, response: Response) {
+    const { id } = request.params;
+
+    try {
+      await this.musicsIntegration.deleteMusic({ id });
+
+      return response.status(HttpStatusCode.OK).send();
 
       //
     } catch (error) {
