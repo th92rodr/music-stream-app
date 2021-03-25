40c385fe5a34cb0858ea7909d52d887b186f5da3
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-25 20:58:18 -0300

    add get-all method in musics-service

--- apis/musics-service/src/services/MusicsService/interface.ts
+++ apis/musics-service/src/services/MusicsService/interface.ts
@@ -9,6 +9,7 @@ import Music from '@entities/Music';
 
 export default interface IMusicsService {
   get(request: GetMusicRequest): Promise<Music>;
+  getAll(): Promise<Array<Music>>;
   create(request: CreateMusicRequest): Promise<Music>;
   update(request: UpdateMusicRequest): Promise<Music>;
   delete(request: DeleteMusicRequest): Promise<void>;
