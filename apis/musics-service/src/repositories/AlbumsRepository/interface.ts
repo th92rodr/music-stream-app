f6768dbe84bb6e423081fc3a94a9d12362e9db70
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-25 20:55:18 -0300

    create albums-repository translators

--- apis/musics-service/src/repositories/AlbumsRepository/interface.ts
+++ apis/musics-service/src/repositories/AlbumsRepository/interface.ts
@@ -2,6 +2,7 @@ import Album from '@entities/Album';
 
 export default interface IAlbumsRepository {
   find(id: string): Promise<Album | undefined>;
+  findAll(): Promise<Array<Album>>;
   store(album: Album): Promise<void>;
   update(album: Album): Promise<void>;
   delete(id: string): Promise<void>;
