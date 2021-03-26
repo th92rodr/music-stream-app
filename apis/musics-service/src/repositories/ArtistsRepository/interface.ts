155f3a20b0b09f13ad1b4fc608a6acf05f6aa5c3
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-03-26 10:26:28 -0300

    add pagination in find-all method in artists-repository

--- apis/musics-service/src/repositories/ArtistsRepository/interface.ts
+++ apis/musics-service/src/repositories/ArtistsRepository/interface.ts
@@ -1,8 +1,9 @@
+import { PaginationRequest } from './dtos';
 import Artist from '@entities/Artist';
 
 export default interface IArtistsRepository {
   find(id: string): Promise<Artist | undefined>;
-  findAll(): Promise<Array<Artist>>;
+  findAll(request?: PaginationRequest): Promise<Array<Artist>>;
   findByGenre(genre: number): Promise<Array<Artist>>;
   store(artist: Artist): Promise<void>;
   update(artist: Artist): Promise<void>;
