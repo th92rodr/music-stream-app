0c34f613ddb923abdb831406ffbdc0835bed4d35
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-04-01 19:13:26 -0300

    musics-service: add fields to artist entity

--- apis/musics-service/src/database/SQL/migrations/20210228153929_CreateArtists.ts
+++ apis/musics-service/src/database/SQL/migrations/20210228153929_CreateArtists.ts
@@ -6,7 +6,10 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(ArtistsTable, table => {
     table.string('id').primary();
     table.string('name').notNullable();
-    table.string('description');
+    table.string('country').notNullable();
+    table.date('foundationDate').notNullable();
+    table.specificType('members', 'text[]');
+    table.string('description', 5000);
     table.integer('genre').notNullable();
     table.specificType('photos', 'text[]');
   });
