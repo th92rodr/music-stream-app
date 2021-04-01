bedc7028258496d3129ea77135743b755b804c16
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-04-01 19:13:26 -0300

    add fields to artist entity

--- apis/musics-service/src/entities/Artist.ts
+++ apis/musics-service/src/entities/Artist.ts
@@ -3,14 +3,20 @@ import Album from './Album';
 export default class Artist {
   public readonly id: string;
   public readonly name: string;
+  public readonly country: string;
+  public readonly foundationDate: Date;
+  public readonly members: Array<string>;
   public readonly description: string;
   public readonly genre: number;
   public readonly photos: Array<string>;
   public albums: Array<Album>;
 
-  constructor({ id, name, description, genre, photos }: Omit<Artist, 'albums' | 'setAlbums'>) {
+  constructor({ id, name, country, foundationDate, members, description, genre, photos }: Omit<Artist, 'albums' | 'setAlbums'>) {
     this.id = id;
     this.name = name;
+    this.country = country;
+    this.foundationDate = foundationDate;
+    this.members = members;
     this.description = description;
     this.genre = genre;
     this.photos = photos;
