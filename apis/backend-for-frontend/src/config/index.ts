00ea90f0170bbd8dec712716b3a506fb2a20db96
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-04-10 19:55:04 -0300

    update storage config var value

--- apis/backend-for-frontend/src/config/index.ts
+++ apis/backend-for-frontend/src/config/index.ts
@@ -33,6 +33,6 @@ export default {
   },
 
   storage: {
-    path: path.resolve(__dirname, '..', '..', '..', '..', 'storage', 'db'),
+    path: path.resolve(__dirname, '..', '..', '..', '..', 'storage'),
   },
 };
