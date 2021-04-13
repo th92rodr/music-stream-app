b1005cac4870e239ed9f2539ea8e654ade3578b1
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-04-13 15:05:31 -0300

    musics-service: create date / timestamp conversion utils functions

--- apis/musics-service/src/utils/index.ts
+++ apis/musics-service/src/utils/index.ts
@@ -3,3 +3,11 @@ export const removeUndefineds = <T>(value: T | undefined): value is T => value !
 export const arrayIntersection = <T>(array1: Array<T>, array2: Array<T>): Array<T> => {
   return array1.filter(element => array2.includes(element));
 };
+
+export const dateToTimestamp = (date: Date): number => {
+  return date.getTime();
+};
+
+export const timestampToDate = (timestamp: number): Date => {
+  return new Date(timestamp);
+};
