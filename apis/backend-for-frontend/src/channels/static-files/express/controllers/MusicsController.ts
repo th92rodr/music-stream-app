aa350d220a53a42e025ac511b8f961390aa970ea
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-04-15 00:10:28 -0300

    bff: update static files server routes

--- apis/backend-for-frontend/src/channels/static-files/express/controllers/MusicsController.ts
+++ apis/backend-for-frontend/src/channels/static-files/express/controllers/MusicsController.ts
@@ -4,35 +4,28 @@ import path from 'path';
 
 import Config from '@config/index';
 import { HttpStatusCode } from '@constants/index';
-import IMusicsIntegration from '@integrations/MusicsIntegration/interface';
-import { getFileStatus } from '@utils/index';
+import { getFileExtension, getFileStatus } from '@utils/index';
 
 export default class MusicsController {
-  private musicsIntegration: IMusicsIntegration;
-
-  constructor(musicsIntegration: IMusicsIntegration) {
-    this.musicsIntegration = musicsIntegration;
-  }
+  constructor() {}
 
   public async stream(request: Request, response: Response) {
-    const { id } = request.params;
+    const { file } = request.query;
 
-    try {
-      const music = await this.musicsIntegration.getMusic({ id });
-
-      const musicFile = path.resolve(Config.storage.path, music.file);
-
-      const fileStatus = await getFileStatus(musicFile);
-
-      response.writeHead(HttpStatusCode.OK, {
-        'Content-Type': 'audio/mp3',
-        'Content-Length': fileStatus.size,
-      });
-
-      const stream = fs.createReadStream(musicFile);
-      stream.pipe(response);
-    } catch (error) {
-      response.end();
+    if (!file) {
+      return response.end();
     }
+
+    const filePath = path.resolve(Config.storage.path, file as string);
+
+    const fileStatus = await getFileStatus(filePath);
+
+    response.writeHead(HttpStatusCode.OK, {
+      'Content-Type': `audio/${getFileExtension(filePath)}`,
+      'Content-Length': fileStatus.size,
+    });
+
+    const stream = fs.createReadStream(filePath);
+    stream.pipe(response);
   }
 }
