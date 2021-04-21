266e549e4e8fb90d8fd214df3cdebbfbddceddc0
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-04-21 11:17:51 -0300

    playlist-service: sql-playlists-repository - implement find method

--- apis/playlists-service/internal/repositories/playlistsRepository/sqlPlaylistsRepository.go
+++ apis/playlists-service/internal/repositories/playlistsRepository/sqlPlaylistsRepository.go
@@ -2,8 +2,16 @@ package playlistsRepository
 
 import (
 	"database/sql"
+	"fmt"
 
+	c "playlists-service/internal/constants"
 	e "playlists-service/internal/entities"
+)
+
+const (
+	fieldId     = "id"
+	fieldName   = "name"
+	fieldUserId = "user_id"
 )
 
 type playlistsRepository struct {
@@ -16,7 +24,31 @@ func New(databaseConnection *sql.DB) IPlaylistsRepository {
 	}
 }
 
-func (r playlistsRepository) Find(id string) (*e.Playlist, map[int32]string, error) {}
+func (r playlistsRepository) Find(id string) (*e.Playlist, map[int32]string, error) {
+	playlist := &e.Playlist{}
+
+	query := fmt.Sprintf(`
+		SELECT * FROM %s WHERE %s = '%s'`,
+		c.PlaylistsTable, fieldId, id,
+	)
+
+	row := r.databaseConnection.QueryRow(query)
+
+	if err := row.Scan(&playlist.Id, &playlist.Name, &playlist.UserId); err != nil {
+		if err == sql.ErrNoRows {
+			customError := c.ErrorPlaylistNotFound
+			customError.Message = fmt.Sprintf("A playlist entity with the id %s was not found.", id)
+			customError.Details = err.Error()
+			return nil, nil, customError
+		}
+
+		customError := c.InternalError
+		customError.Details = err.Error()
+		return nil, nil, customError
+	}
+
+	return playlist, nil, nil
+}
 
 func (r playlistsRepository) FindAll(userId string) ([]e.Playlist, error) {}
 
