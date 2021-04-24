2f98b7b53503ad8dadb5a803b0200836de2dc69b
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-04-24 19:13:32 -0300

    playlist-service: implement create method

---
+++ apis/playlists-service/internal/services/playlistsService/playlistsService.go
@@ -0,0 +1,48 @@
+package playlistsService
+
+import (
+	e "playlists-service/internal/entities"
+	m "playlists-service/internal/integrations/musicsIntegration"
+	i "playlists-service/internal/providers/idProvider"
+	r "playlists-service/internal/repositories/playlistsRepository"
+)
+
+type playlistsService struct {
+	idProvider          i.IIdProvider
+	musicsIntegration   m.IMusicsIntegration
+	playlistsRepository r.IPlaylistsRepository
+}
+
+func New(idProvider i.IIdProvider, musicsIntegration m.IMusicsIntegration, playlistsRepository r.IPlaylistsRepository) IPlaylistsService {
+	return playlistsService{
+		idProvider:          idProvider,
+		musicsIntegration:   musicsIntegration,
+		playlistsRepository: playlistsRepository,
+	}
+}
+
+func (s playlistsService) Get(request GetPlaylistRequest) (*e.Playlist, error) {}
+
+func (s playlistsService) GetAll(request GetAllPlaylistsRequest) ([]e.Playlist, error) {}
+
+func (s playlistsService) Create(request CreatePlaylistRequest) (*e.Playlist, error) {
+	playlist := &e.Playlist{
+		Id:     s.idProvider.Generate(),
+		Name:   request.Name,
+		UserId: request.UserId,
+	}
+
+	if err := s.playlistsRepository.Store(r.StorePlaylistRequest{
+		Id:     playlist.Id,
+		Name:   playlist.Name,
+		UserId: playlist.UserId,
+	}); err != nil {
+		return nil, err
+	}
+
+	return playlist, nil
+}
+
+func (s playlistsService) Update(request UpdatePlaylistRequest) (*e.Playlist, error) {}
+
+func (s playlistsService) Delete(request DeletePlaylistRequest) error {}
