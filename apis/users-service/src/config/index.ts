c07771591b2393f58999bf671526608da3dda097
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-01 12:09:16 -0300

    add playlists-service address in bff config

--- apis/users-service/src/config/index.ts
+++ apis/users-service/src/config/index.ts
@@ -7,7 +7,7 @@ export default {
   channels: {
     grpc: {
       host: '0.0.0.0',
-      port: Number(process.env.GRPC_PORT) || 6560,
+      port: Number(process.env.GRPC_PORT) || 6569,
     },
   },
 
