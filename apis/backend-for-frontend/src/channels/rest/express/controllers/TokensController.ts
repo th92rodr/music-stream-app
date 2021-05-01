be9e1a579999a1950b8acc45aca291c7290a25c5
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-01 13:53:56 -0300

    call validator middleware in users and tokens controllers

--- apis/backend-for-frontend/src/channels/rest/express/controllers/TokensController.ts
+++ apis/backend-for-frontend/src/channels/rest/express/controllers/TokensController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 
+import Validator from '@channels/rest/middlewares/Validator';
 import { HttpStatusCode } from '@constants/index';
 import BaseError from '@constants/BaseError';
 import { InternalError } from '@constants/errors';
@@ -7,20 +8,25 @@ import IUsersIntegration from '@integrations/UsersIntegration/interface';
 
 export default class TokensController {
   private usersIntegration: IUsersIntegration;
+  private validator: Validator;
 
-  constructor(usersIntegration: IUsersIntegration) {
+  constructor(usersIntegration: IUsersIntegration, validator: Validator) {
     this.usersIntegration = usersIntegration;
+    this.validator = validator;
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
+    const errors = this.validator.validateCreateTokenRequest(request.body);
+    if (errors.length > 0) {
+      return response.status(HttpStatusCode.BAD_REQUEST).json({ errors });
+    }
+
     const { email, password } = request.body;
 
     try {
       const { token, user } = await this.usersIntegration.authenticateUser({ email, password });
 
       return response.status(HttpStatusCode.OK).json({ token, user });
-
-      //
     } catch (error) {
       if (error instanceof BaseError) {
         return response.status(error.statusCode).json({ error: error.message });
