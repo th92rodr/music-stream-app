0ab1818420480a086e2fc56db47bc1bd2937b614
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-04 15:45:51 -0300

    playlist-service: add tracks grpc handler methods

--- apis/playlists-service/internal/channels/grpc/handlers.go
+++ apis/playlists-service/internal/channels/grpc/handlers.go
@@ -79,3 +79,50 @@ func (c grpcChannel) DeletePlaylist(ctx context.Context, request *proto.DeletePl
 
 	return &proto.Empty{}, nil
 }
+
+func (c grpcChannel) AddTrack(ctx context.Context, request *proto.AddTrackRequest) (*proto.Track, error) {
+	track, err := c.playlistsService.AddTrack(s.AddTrackRequest{
+		UserId:     request.UserId,
+		PlaylistId: request.PlaylistId,
+		MusicId:    request.MusicId,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	c.loggerProvider.Info("ADD TRACK", nil)
+
+	return translatePlaylistTrack(track), nil
+}
+
+func (c grpcChannel) UpdateTrack(ctx context.Context, request *proto.UpdateTrackRequest) (*proto.Track, error) {
+	track, err := c.playlistsService.UpdateTrack(s.UpdateTrackRequest{
+		UserId:     request.UserId,
+		PlaylistId: request.PlaylistId,
+		Id:         request.Id,
+		Index:      request.Index,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	c.loggerProvider.Info("UPDATE TRACK", request.Id)
+
+	return translatePlaylistTrack(track), nil
+}
+
+func (c grpcChannel) RemoveTrack(ctx context.Context, request *proto.RemoveTrackRequest) (*proto.Empty, error) {
+	if err := c.playlistsService.RemoveTrack(s.RemoveTrackRequest{
+		UserId:     request.UserId,
+		PlaylistId: request.PlaylistId,
+		Id:         request.Id,
+	}); err != nil {
+		return nil, err
+	}
+
+	c.loggerProvider.Info("DELETE TRACK", request.Id)
+
+	return &proto.Empty{}, nil
+}
