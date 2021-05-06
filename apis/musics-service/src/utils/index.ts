0693753527e67f6426aed9c62f65902893ab945b
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-06 15:18:27 -0300

    musics service: handle date type variables

--- apis/musics-service/src/utils/index.ts
+++ apis/musics-service/src/utils/index.ts
@@ -4,10 +4,20 @@ export const arrayIntersection = <T>(array1: Array<T>, array2: Array<T>): Array<
   return array1.filter(element => array2.includes(element));
 };
 
+export const newDate = (str: string): Date => {
+  return withoutTime(new Date(str));
+};
+
 export const dateToTimestamp = (date: Date): number => {
   return date.getTime();
 };
 
 export const timestampToDate = (timestamp: number): Date => {
-  return new Date(timestamp);
+  return withoutTime(new Date(timestamp));
 };
+
+function withoutTime(dateTime: Date) {
+  const date = new Date(dateTime.getTime());
+  date.setUTCHours(12, 0, 0, 0);
+  return date;
+}
