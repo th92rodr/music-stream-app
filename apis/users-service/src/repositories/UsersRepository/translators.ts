aa4e9ec8c51f38a260026430703c3d736aecb2cf
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-06 16:01:48 -0300

    users-service: add users-db entity in users-repository

--- apis/users-service/src/repositories/UsersRepository/translators.ts
+++ apis/users-service/src/repositories/UsersRepository/translators.ts
@@ -1,6 +1,7 @@
+import { UsersDb } from './SQLUsersRepository';
 import User from '@entities/User';
 
-export function translateUser(user: User): User {
+export function translateUser(user: UsersDb): User {
   return new User({
     id: user.id,
     username: user.username,
