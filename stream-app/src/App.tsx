8620602b41d5b3b283b3373a7320d454d6448b79
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-16 13:24:47 -0300

    stream-app: add global style and sass variables

--- stream-app/src/App.tsx
+++ stream-app/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Routes } from './routes';
 
+import './styles/global.scss';
+
 export const App: React.FC = () => (
   <BrowserRouter>
     <Routes />;
