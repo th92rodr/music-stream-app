d3516d13cb4c29da592b45a0a40ae316ed849f09
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-19 16:58:26 -0300

    stream-app: create Artist page

--- stream-app/src/routes/index.tsx
+++ stream-app/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { ArtistPage } from '../pages/ArtistPage';
 import { MainPage } from '../pages/MainPage';
 
 export const Routes: React.FC = () => (
   <Switch>
     <Route path='/' exact component={MainPage} />
+    <Route path='/artists/:artist+' component={ArtistPage} />
   </Switch>
 );
