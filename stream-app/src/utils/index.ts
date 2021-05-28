fbaf30d9140c2dc1edf20dafac65fd21ebeb70a6
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-28 12:05:09 -0300

    stream-app: add convert-duration-to-time-string util function

--- stream-app/src/utils/index.ts
+++ stream-app/src/utils/index.ts
@@ -3,3 +3,5 @@ import { staticFilesAddress } from '../services/api';
 export function staticFilesUrl(queryParam: string): string {
   return `${staticFilesAddress}/files?file=${queryParam}`;
 }
+
+export { convertDurationToTimeString } from './conversions';
