021c80f6ccca77a9a81e2aa281670e4db5a97962
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-05-30 20:09:26 -0300

    bff: create musics-views rest channel controller

--- apis/backend-for-frontend/src/channels/index.ts
+++ apis/backend-for-frontend/src/channels/index.ts
@@ -2,9 +2,9 @@ import ExpressRestChannel from './rest/express/Express';
 import ExpressStaticFilesChannel from './static-files/express/Express';
 
 import { errorHandler } from '@handlers/index';
-import { musicsIntegration, playlistsIntegration, usersIntegration } from '@integrations/index';
+import { musicsIntegration, playlistsIntegration, usersIntegration, usersMusicsIntegration } from '@integrations/index';
 import { loggerProvider } from '@providers/index';
 
-export const restChannel = new ExpressRestChannel(musicsIntegration, playlistsIntegration, usersIntegration, errorHandler, loggerProvider);
+export const restChannel = new ExpressRestChannel(musicsIntegration, playlistsIntegration, usersIntegration, usersMusicsIntegration, errorHandler, loggerProvider);
 
 export const staticFilesChannel = new ExpressStaticFilesChannel(musicsIntegration, errorHandler, loggerProvider);
