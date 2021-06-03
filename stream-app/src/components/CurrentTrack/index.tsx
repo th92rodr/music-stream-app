79a41546a41c62277b53579b69e3fdc41f5e3ddf
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-06-03 20:24:21 -0300

    stream app web: update current-track component

--- stream-app/src/components/CurrentTrack/index.tsx
+++ stream-app/src/components/CurrentTrack/index.tsx
@@ -11,23 +11,17 @@ import './styles.scss';
 // icons
 import { ReactComponent as IconArrowBackward } from '../../assets/icons/icon-arrow-backward.svg';
 import { ReactComponent as IconArrowForward } from '../../assets/icons/icon-arrow-forward.svg';
+import { ReactComponent as IconList } from '../../assets/icons/icon-list.svg';
 import { ReactComponent as IconPause } from '../../assets/icons/icon-pause.svg';
 import { ReactComponent as IconPlay } from '../../assets/icons/icon-play-filled.svg';
+import { ReactComponent as IconVolume } from '../../assets/icons/icon-volume.svg';
 
 export const CurrentTrack: React.FC = () => {
   const [progress, setProgress] = useState(0);
 
   const audioRef = createRef<HTMLAudioElement>();
 
-  const {
-    musicsList,
-    currentMusicIndex,
-    isPlaying,
-    togglePlay,
-    setPlayingState,
-    playPrevious,
-    playNext,
-  } = usePlayer();
+  const { musicsList, currentMusicIndex, isPlaying, togglePlay, setPlayingState, playPrevious, playNext } = usePlayer();
 
   const music = musicsList[currentMusicIndex];
 
@@ -85,9 +79,7 @@ export const CurrentTrack: React.FC = () => {
             </div>
 
             <div className='current__track__progress'>
-              <div className='current__track__progress__start'>
-                {convertDurationToTimeString(progress)}
-              </div>
+              <div className='current__track__progress__start'>{convertDurationToTimeString(progress)}</div>
               <div className='current__track__progress__slider'>
                 <Slider
                   value={progress}
@@ -98,9 +90,7 @@ export const CurrentTrack: React.FC = () => {
                   handleStyle={{ borderColor: '#176087', borderWidth: 4 }}
                 />
               </div>
-              <div className='current__track__progress__finish'>
-                {convertDurationToTimeString(music.duration)}
-              </div>
+              <div className='current__track__progress__finish'>{convertDurationToTimeString(music.duration)}</div>
             </div>
           </div>
 
@@ -115,13 +105,59 @@ export const CurrentTrack: React.FC = () => {
           />
 
           <div className='current__track__lyrics'>
-            <a href='#' className='lyrics'>
-              Lyrics
-            </a>
+            <button className='volume'>
+              <IconVolume />
+            </button>
+
+            <button className='lyrics'>
+              <IconList />
+            </button>
           </div>
         </section>
       ) : (
-        <div></div>
+        <section className='current__track'>
+          <div className='current__track__name'>
+            <div className='current__track__name__music'></div>
+            <div className='current__track__name__band'></div>
+          </div>
+
+          <div className='current__track__progress__wrapper'>
+            <div className='current__track__actions'>
+              <a>
+                <IconArrowBackward />
+              </a>
+              <a className='play'>
+                <IconPlay />
+              </a>
+              <a>
+                <IconArrowForward />
+              </a>
+            </div>
+
+            <div className='current__track__progress'>
+              <div className='current__track__progress__start'>--:--</div>
+              <div className='current__track__progress__slider'>
+                {/* prettier-ignore */}
+                <Slider
+                  trackStyle={{ backgroundColor: '#176087' }}
+                  railStyle={{ backgroundColor: '#8080804d' }}
+                  handleStyle={{ borderColor: '#176087', borderWidth: 4 }}
+                />
+              </div>
+              <div className='current__track__progress__finish'>--:--</div>
+            </div>
+          </div>
+
+          <div className='current__track__lyrics'>
+            <button className='volume'>
+              <IconVolume />
+            </button>
+
+            <button className='lyrics'>
+              <IconList />
+            </button>
+          </div>
+        </section>
       )}
     </>
   );
