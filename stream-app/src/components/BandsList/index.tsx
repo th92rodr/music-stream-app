7c4d2e3ba76f7c604570b69e3ddf468473c4c3a5
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-07-03 12:41:36 -0300

    stream app web: add filter by genre in bands list component

--- stream-app/src/components/BandsList/index.tsx
+++ stream-app/src/components/BandsList/index.tsx
@@ -12,37 +12,70 @@ export const BandsList: React.FC = () => {
   const history = useHistory();
 
   const [bands, setBands] = useState<Band[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<string>('');
+
+  const genres = ['Heavy Metal', 'Folk Metal', 'Power Metal', 'Death Metal', 'Thrash Metal', 'Black Metal'];
 
   useEffect(() => {
-    api.get('/artists').then(response => setBands(response.data));
+    api.get('/artists?limit=100').then(response => setBands(response.data));
   }, []);
 
-  function handleBandClick(bandId: string) {
-    history.push(`/artists/${bandId}`);
+  function selectArtist(id: string) {
+    history.push(`/artists/${id}`);
+  }
+
+  function filterArtistsByGenre(genre: string) {
+    if (genre == selectedGenre) {
+      setSelectedGenre('');
+    } else {
+      setSelectedGenre(genre);
+    }
   }
 
   return (
     <div className='bands'>
       <div className='bands__header'>
-        <h2>Bands</h2>
+        <h2>Artists</h2>
       </div>
 
+      <ul className='bands__genre__list'>
+        {genres.map(genre => {
+          return (
+            // prettier-ignore
+            <li
+              className={genre == selectedGenre ? 'bands__genre__item active' : 'bands__genre__item'}
+              key={genre}
+            >
+              <button onClick={() => filterArtistsByGenre(genre)}>
+                {genre}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+
       <ul className='bands__list'>
-        {bands.map(band => (
-          <li
-            className='bands__list__item'
-            key={band.id}
-            onClick={() => handleBandClick(band.id)}
-            style={{
-              backgroundImage: `url(${staticFilesUrl(band.profile_img)}`,
-            }}
-          >
-            <div className='bands__list__item__info'>
-              <span className='band__name'>{band.name}</span>
-              <span className='band__genre'>{band.genre}</span>
-            </div>
-          </li>
-        ))}
+        {bands.map(band => {
+          if (selectedGenre != '' && band.genre != selectedGenre) {
+            return;
+          }
+
+          return (
+            <li
+              className='bands__list__item'
+              key={band.id}
+              onClick={() => selectArtist(band.id)}
+              style={{
+                backgroundImage: `url(${staticFilesUrl(band.profile_img)}`,
+              }}
+            >
+              <div className='bands__list__item__info'>
+                <span className='band__name'>{band.name}</span>
+                <span className='band__genre'>{band.genre}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
