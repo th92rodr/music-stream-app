0bdc408613c59f372471124d13c9d49d3dd3b639
th92rodr <45070690+th92rodr@users.noreply.github.com>
2021-07-03 12:39:55 -0300

    stream app web: update static files address

--- stream-app/src/services/api.ts
+++ stream-app/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiAddress = 'http://localhost:8080/api';
-export const staticFilesAddress = 'http://localhost:9090/web';
+export const staticFilesAddress = 'http://localhost:9090/static';
 
 export const api = axios.create({
   baseURL: apiAddress,
